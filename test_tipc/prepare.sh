10d3deca5d4caca4e5772f619da99eb9cbc1b3b3
langdu <1207540056@qq.com>
2021-11-27 15:24:31 +0800

    update prepare.sh

--- test_tipc/prepare.sh
+++ test_tipc/prepare.sh
@@ -2,9 +2,7 @@
 source test_tipc/common_func.sh
 
 FILENAME=$1
-# MODE be one of ['lite_train_lite_infer' 'lite_train_whole_infer' 'whole_train_whole_infer',
-#                 'whole_infer', 'klquant_whole_infer',
-#                 'cpp_infer', 'serving_infer']
+# MODE be one of ['lite_train_infer']
 MODE=$2
 
 dataline=$(cat ${FILENAME})
@@ -18,8 +16,7 @@ model_name=$(func_parser_value "${lines[1]}")
 
 trainer_list=$(func_parser_value "${lines[14]}")
 
-# MODE be one of ['lite_train_infer' 'whole_infer' 'whole_train_infer']
-MODE=$2
+# MODE be one of ['lite_train_infer']
 if [ ${MODE} = "lite_train_infer" ];then
     rm -rf ./test_tipc/data/mini_supervisely
     wget -nc -P ./test_tipc/data/ https://paddleseg.bj.bcebos.com/humanseg/data/mini_supervisely.zip
