c64c70fc464f95dc81c9a8a870f4a7af717022d3
langdu <1207540056@qq.com>
2021-11-27 15:19:28 +0800

    refine prepare.sh

--- test_tipc/prepare.sh
+++ test_tipc/prepare.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 source test_tipc/common_func.sh
 
-set -o errexit
-set -o nounset
-
 FILENAME=$1
 # MODE be one of ['lite_train_lite_infer' 'lite_train_whole_infer' 'whole_train_whole_infer',
 #                 'whole_infer', 'klquant_whole_infer',
@@ -15,6 +12,7 @@ dataline=$(cat ${FILENAME})
 # parser params
 IFS=$'\n'
 lines=(${dataline})
+
 # The training params
 model_name=$(func_parser_value "${lines[1]}")
 
@@ -22,12 +20,8 @@ trainer_list=$(func_parser_value "${lines[14]}")
 
 # MODE be one of ['lite_train_infer' 'whole_infer' 'whole_train_infer']
 MODE=$2
-if [ ${MODE} = "infer" ];then
-    model_path=test_tipc/output/norm_gpus_0_autocast_null/
-    rm -rf $model_path
-    wget -nc -P $model_path https://paddleseg.bj.bcebos.com/dygraph/humanseg/train/fcn_hrnetw18_small_v1_humanseg_192x192.zip
-    cd $model_path && unzip fcn_hrnetw18_small_v1_humanseg_192x192.zip && mv fcn_hrnetw18_small_v1_humanseg_192x192/model.pdparams . && cd ../../../
+if [ ${MODE} = "lite_train_infer" ];then
+    rm -rf ./test_tipc/data/mini_supervisely
+    wget -nc -P ./test_tipc/data/ https://paddleseg.bj.bcebos.com/humanseg/data/mini_supervisely.zip
+    cd ./test_tipc/data/ && unzip mini_supervisely.zip && cd ../../
 fi
-rm -rf ./test_tipc/data/mini_supervisely
-wget -nc -P ./test_tipc/data/ https://paddleseg.bj.bcebos.com/humanseg/data/mini_supervisely.zip
-cd ./test_tipc/data/ && unzip mini_supervisely.zip && cd ../../
